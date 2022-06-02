3dd484834f071d6021a15d6c88862b73bc9073e8
Tariq Muhammad <tariqmuhammad.appelit@nextbridge.com>
2022-06-02 15:10:13 +0500

    removed unnecessary components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OurFeaturesComponent } from './pages/our-features/our-features.component';
+const routes: Routes = [
+  { path: '', component: OurFeaturesComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'features', component: OurFeaturesComponent },
 
-const routes: Routes = [];
+
+  // for invalid path redirect to home page
+  { path: '**', redirectTo: '' },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OurFeaturesComponent } from './pages/our-features/our-features.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactUsComponent,
+    HomeComponent,
+    OurFeaturesComponent,
+    NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
